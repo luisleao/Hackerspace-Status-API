954daa4780c1d9847a60ed8b6920299c4d0b5edb
Thiago Lechuga <thiagoalz@gmail.com>
2013-04-28 00:30:03 +0000

    Making the code fault tolerant

--- Firmware/status_raspberry.py
+++ Firmware/status_raspberry.py
@@ -5,6 +5,7 @@ import urllib2
 import time
 import datetime
 import os
+import sys
 
 host = "http://garoahc.appspot.com";
 location_opened = "/rest/status/open";
@@ -14,7 +15,7 @@ token = "/1234";
 
 sensor_pin = 7
 pinStatus = True
-updateDelay = 600 #10 minutes delay
+updateDelay = 300 #5 minutes delay
 lastUpdate=0
 
 #SETUP
@@ -25,11 +26,10 @@ def update_status():
         global pinStatus
         global lastUpdate
 
-        pinStatus = GPIO.input(sensor_pin)
-        lastUpdate = time.time()
+        readStatus = GPIO.input(sensor_pin)
 
         status_url=host
-        if(GPIO.input(sensor_pin)):
+        if(readStatus):
                 #Open
                 print "Opened"
                 status_url+=location_opened
@@ -40,6 +40,9 @@ def update_status():
 
         status_url+=token
         response = urllib2.urlopen(status_url)
+
+	pinStatus = readStatus
+        lastUpdate = time.time()
 
 def update_macs():
         #macs_command = "sudo nmap -sP 192.168.1.1-154 | egrep -o ..:..:..:..:..:.."
@@ -53,17 +56,20 @@ def update_macs():
         macs_str = macs_str.replace("\n","_")
         macs_str = macs_str[:-1]
 
+	#TODO: Update do post
         macs_url=host+location_macs+"/"+macs_str+token
         print "Updating Macs: "+macs_str
         response = urllib2.urlopen(macs_url)
-        #print macs_url
 
 
 while True:
-        #Status changed or last change was to long ago.
-        if ( (pinStatus != GPIO.input(sensor_pin)) or ( (time.time()-lastUpdate) >= updateDelay ) ):
-                print datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-                print "Updating Status"                
-                update_status()
-                update_macs()
-        time.sleep(30)
+        try:
+		#Status changed or last change was to long ago.
+	        if ( (pinStatus != GPIO.input(sensor_pin)) or ( (time.time()-lastUpdate) >= updateDelay ) ):
+        	        print datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+                	print "Updating Status"                
+	                update_status()
+        	        update_macs()
+	        time.sleep(5)
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
