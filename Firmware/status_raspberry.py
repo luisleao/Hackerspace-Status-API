fb2e5ff5e768d8e2ec9b6329a135509562cfccaf
Thiago Lechuga <thiagoalz@gmail.com>
2013-05-01 21:37:41 +0000

    Getting mac information from router

--- Firmware/status_raspberry.py
+++ Firmware/status_raspberry.py
@@ -6,6 +6,8 @@ import time
 import datetime
 import os
 import sys
+import xml.dom.minidom
+from xml.dom.minidom import Node
 
 host = "http://garoahc.appspot.com";
 location_opened = "/rest/status/open";
@@ -44,26 +46,48 @@ def update_status():
 	pinStatus = readStatus
         lastUpdate = time.time()
 
-def update_macs():
-        #macs_command = "sudo nmap -sP 192.168.1.1-154 | egrep -o ..:..:..:..:..:.."
-        macs_command = "sudo arp-scan --interface eth0 -l | egrep -o ..:..:..:..:..:.."
+def get_parametro_xml(item, parametroName):
+        parametro = item.getElementsByTagName(parametroName)
+        parametro = parametro[0]
+        for atributo in parametro.childNodes:
+                if atributo.nodeType == Node.TEXT_NODE:
+                        return atributo.data
+def get_macs():
+	######Get Data From Router#####
+	macsXml = urllib2.urlopen("http://192.168.1.1/ajaxQueryDevice.gch")
+	macsXml = xml.dom.minidom.parse(macsXml)
+	itensList = macsXml.getElementsByTagName('Parameters')
 
-        cmd = os.popen(macs_command)
-        macs_str= cmd.read()
-        cmd.close()
+	macs_str = ""
+	for iten in itensList:
+        	macs_str = macs_str + get_parametro_xml(iten, "MACAddress") + "_"
+	##########
+
+	#####Network Scan#####
+	##macs_command = "sudo nmap -sP 192.168.1.1-154 | egrep -o ..:..:..:..:..:.."
+        #macs_command = "sudo arp-scan --interface eth0 -l | egrep -o ..:..:..:..:..:.."
+
+        #cmd = os.popen(macs_command)
+        #macs_str= cmd.read()
+        #cmd.close()
+	#macs_str = macs_str.replace("\n","_")
+	##########
 
         macs_str = macs_str.replace(":","")
-        macs_str = macs_str.replace("\n","_")
         macs_str = macs_str[:-1]
+
+	return macs_str
+
+def update_macs():
+	macs_str = get_macs()
 
 	#TODO: Update do post
         macs_url=host+location_macs+"/"+macs_str+token
         print "Updating Macs: "+macs_str
         response = urllib2.urlopen(macs_url)
 
-
 while True:
-        try:
+	try:
 		#Status changed or last change was to long ago.
 	        if ( (pinStatus != GPIO.input(sensor_pin)) or ( (time.time()-lastUpdate) >= updateDelay ) ):
         	        print datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
