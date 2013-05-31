f5527564b780ea9ba8d18c14494a008df1d113dd
Thiago Lechuga <thiagoalz@gmail.com>
2013-05-30 22:15:05 -0300

    Accumulating macs get between update times.

--- Firmware/status_raspberry.py
+++ Firmware/status_raspberry.py
@@ -6,8 +6,6 @@ import time
 import datetime
 import os
 import sys
-import xml.dom.minidom
-from xml.dom.minidom import Node
 
 host = "http://garoahc.appspot.com";
 location_opened = "/rest/status/open";
@@ -19,83 +17,72 @@ sensor_pin = 7
 pinStatus = True
 updateDelay = 300 #5 minutes delay
 lastUpdate=0
+macs = []
 
 #SETUP
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def update_status():
-        global pinStatus
-        global lastUpdate
+    global pinStatus
+    global lastUpdate
 
-        readStatus = GPIO.input(sensor_pin)
+    readStatus = GPIO.input(sensor_pin)
 
-        status_url=host
-        if(readStatus):
-                #Open
-                print "Opened"
-                status_url+=location_opened
-        else:
-                #Closed
-                print "Closed"
-                status_url+=location_closed
+    status_url=host
+    if(readStatus):
+        #Open
+        print "Opened"
+        status_url+=location_opened
+    else:
+        #Closed
+        print "Closed"
+        status_url+=location_closed
 
-        status_url+=token
-        response = urllib2.urlopen(status_url)
+    status_url+=token
+    response = urllib2.urlopen(status_url)
 
-	pinStatus = readStatus
-        lastUpdate = time.time()
+    pinStatus = readStatus
+    lastUpdate = time.time()
 
-def get_parametro_xml(item, parametroName):
-        parametro = item.getElementsByTagName(parametroName)
-        parametro = parametro[0]
-        for atributo in parametro.childNodes:
-                if atributo.nodeType == Node.TEXT_NODE:
-                        return atributo.data
 def get_macs():
-	######Get Data From Router#####
-	#macsXml = urllib2.urlopen("http://192.168.1.1/ajaxQueryDevice.gch")
-	#macsXml = xml.dom.minidom.parse(macsXml)
-	#itensList = macsXml.getElementsByTagName('Parameters')
+    #####Network Scan#####
+    #macs_command = "sudo nmap -sP 192.168.1.1-154 | egrep -o ..:..:..:..:..:.."
+    macs_command = "sudo arp-scan --interface eth0 -l | egrep -o ..:..:..:..:..:.."
 
-	#macs_str = ""
-	#for iten in itensList:
-        #	macs_str = macs_str + get_parametro_xml(iten, "MACAddress") + "_"
-	##########
+    cmd = os.popen(macs_command)
+    macs_str= cmd.read()
+    cmd.close()
+    
+    macs_str = macs_str[:-1]
+    ##########
 
-	#####Network Scan#####
-	#macs_command = "sudo nmap -sP 192.168.1.1-154 | egrep -o ..:..:..:..:..:.."
-        macs_command = "sudo arp-scan --interface eth0 -l | egrep -o ..:..:..:..:..:.."
+    return macs_str.split('\n')
 
-        cmd = os.popen(macs_command)
-        macs_str= cmd.read()
-        cmd.close()
-	macs_str = macs_str.replace("\n","_")
-	##########
-
-        macs_str = macs_str.replace(":","")
-        macs_str = macs_str[:-1]
-
-	return macs_str
-
-def update_macs():
-	macs_str = get_macs()
-
-	#TODO: Update to post
-        macs_url=host+location_macs+"/"+macs_str+token
-        print "Updating Macs: "+macs_str
-        response = urllib2.urlopen(macs_url)
+def update_macs(macsl):
+    macs_str = "_".join(macsl)
+    macs_str = macs_str.replace(":","")
+    
+    #TODO: Update to post
+    macs_url=host+location_macs+"/"+macs_str+token
+    print "Updating Macs: "+macs_str
+    response = urllib2.urlopen(macs_url)
 
 while True:
-	try:
-		#Status changed or last change was to long ago.
-	        if ( (pinStatus != GPIO.input(sensor_pin)) or ( (time.time()-lastUpdate) >= updateDelay ) ):
-        	        print datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-                	print "Updating Status"                
-	                update_status()
-        	        update_macs()
-	        time.sleep(5)
-	except (KeyboardInterrupt, SystemExit):
-		raise
-	except Exception,e:
-                print str(e)
+    try:
+        macs=macs + get_macs()
+        macs = list(set(macs))
+        print "Number of Macs: " + len(macs)
+        
+        #Status changed or last change was to long ago.
+        if ( (pinStatus != GPIO.input(sensor_pin)) or ( (time.time()-lastUpdate) >= updateDelay ) ):
+            print datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+            print "Updating Status"
+            update_status()
+            update_macs(macs)
+            macs=[]
+        time.sleep(10)
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except Exception,e:
+        print str(e)
